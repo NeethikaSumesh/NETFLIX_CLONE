85568639987004d87a06afcfb52c62692a8ff32c
Neethika Sumesh <neethikasumesh@gmail.com>
2023-05-23 00:25:24 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import NavBar from './Components/NavBar/NavBar';
+import {originals,action} from './urls'
+import './App.css'
+import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <RowPost url={originals} title='Netflix Originals' />
+      <RowPost url={action} title='Action Movies' isSmall />
+      <RowPost url={action} title='Action Movies' isSmall />
+
+      
+    </div>
+  );
+}
+
+export default App
